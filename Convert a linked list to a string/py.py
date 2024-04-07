d87b61a668199980da364d18f40f6766f0def086
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-04-07 17:16:45 +0300

    make Convert a linked list to a string

--- Convert a linked list to a string/py.py
+++ Convert a linked list to a string/py.py
@@ -1,2 +1,11 @@
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
 def stringify(node):
-    pass
+    if node is None:
+        return "None"
+    return f"{node.data} -> {stringify(node.next)}"
+
+assert stringify(Node(1, Node(2, Node(3)))) == "1 -> 2 -> 3 -> None"
