02a1857237dea8572c3d5451f2deeeebe8d5e635
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-04-08 13:08:23 +0300

    firasr attempt to move node

---
+++ Linked Lists - Move Node/py.py
@@ -0,0 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    if source is None:
+        raise ValueError
+    node = source
+    source = source.next
+    node.next = dest
+    # Remember to return the context.
+    return Context(source, dest)
