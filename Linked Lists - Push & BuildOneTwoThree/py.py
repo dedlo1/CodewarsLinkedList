a25166f756a9e562c507aa84268dad2141e6eabf
Artem Krutko <krutko.pn@ucu.edu.ua>
2024-04-08 11:20:23 +0300

    use template from codewars to Linked Lists - Push

---
+++ Linked Lists - Push & BuildOneTwoThree/py.py
@@ -0,0 +1,18 @@
+from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+    
+def push(head, data):
+    # Your code goes here.
+    return Node(None)
+
+def build_one_two_three():
+    # Your code goes here.
+    return Node(None)
